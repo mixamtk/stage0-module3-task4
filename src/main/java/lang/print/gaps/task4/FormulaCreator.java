b8f120bd4a76b97718b57eaf55697e46280af2d3
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-06-23 10:30:47 +0300

    Update FormulaCreator.java

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -4,6 +4,6 @@ public class FormulaCreator {
     public static void main(String[] args) {
         int a = 1;
         int b = 1;
-        float r = (9 * a * a - 5 - b + 2 + a - 7)*((a + b - 4 * a * b) / 2);
+        double r = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
